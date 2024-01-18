859f512055444dedc8ce020bf441343939b62c78
Eugen <eugen.hoppe.de@gmail.com>
2024-01-18 23:53:19 +0100

    font constants

--- letter.py
+++ letter.py
@@ -1,8 +1,10 @@
 from app.envelope import Envelope
+from app.constants import Font
 
 
 if __name__ == "__main__":
     doc = Envelope()
     doc.add_page()
     doc.envelope_marks()
+
     doc.output('output/envelope.pdf')
