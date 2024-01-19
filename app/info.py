bb7cb792d87716760381206bd2b67c4756b150a3
Eugen <eugen.hoppe.de@gmail.com>
2024-01-19 18:38:45 +0100

    account info field

--- app/info.py
+++ app/info.py
@@ -3,5 +3,17 @@ from app.label import Label
 
 
 class Info(Label):
-    pass
-
+    lb_xy_info: tuple[int, int] = (125, 50)
+    
+    def information(self):
+        self.set_xy(*self.lb_xy_info)
+        self.set_font(**self.options_font.P1)
+        self.multi_cell(
+            w=80,
+            h=5,
+            txt=self.conf_account().address(sep="\n")
+        )
+    
+    def info_header(self):
+        self.lb_header()
+        self.information()
--- letter.py
+++ letter.py
@@ -1,27 +1,40 @@
-from app import envelope, letterhead, label
+from app import envelope, letterhead, label, info
 
 
 if __name__ == "__main__":
-    example_pdfs, examle_id = {}, 3
+    example_pdfs, examle_id = {}, 4
+
     if examle_id == 1:
         example_pdfs[examle_id] = "envelope.pdf"
         doc = envelope.Envelope()
         doc.add_page()
         doc.envelope_marks()
+
     if examle_id == 2:
         example_pdfs[examle_id] = "letterhead.pdf"
         doc = letterhead.Letterhead()
         doc.add_page()
         doc.lh_header()
+
+    label_recipient_address = [
+        "d.lb_recipient_address.name",
+        "d.lb_recipient_address.second_address_line",
+        "d.lb_recipient_address.street",
+        "d.lb_recipient_address.postal_code_and_city",
+    ]
+
     if examle_id == 3:
         example_pdfs[examle_id] = "label.pdf"
         doc = label.Label()
-        doc.lb_recipient_address = [
-            "d.lb_recipient_address.name",
-            "d.lb_recipient_address.second_address_line",
-            "d.lb_recipient_address.street",
-            "d.lb_recipient_address.postal_code_and_city",
-        ]
+        doc.lb_recipient_address = label_recipient_address
         doc.add_page()
         doc.lb_header()
+
+    if examle_id == 4:
+        example_pdfs[examle_id] = "info.pdf"
+        doc = info.Info()
+        doc.lb_recipient_address = label_recipient_address
+        doc.add_page()
+        doc.info_header()
+
     doc.output(doc.const_path().PDF + example_pdfs[examle_id])
